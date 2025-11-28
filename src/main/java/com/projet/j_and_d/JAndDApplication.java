346803c909d71069a5ac919f7b1331d2b5569bd3
theblouch <thibaud.reigner@gmail.com>
2025-11-28 17:38:48 +0100

    Ajout de Springboot

---
+++ src/main/java/com/projet/j_and_d/JAndDApplication.java
@@ -0,0 +1,13 @@
+package com.projet.j_and_d;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JAndDApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JAndDApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projet/j_and_d/JAndDApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projet.j_and_d;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JAndDApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
