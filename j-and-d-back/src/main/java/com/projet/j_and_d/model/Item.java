c80dd502fd2f46aca572bf06977fa59cad82eed1
Bayshunpu <nolwen.berton@outlook.fr>
2025-12-01 11:36:23 +0100

    Adapt items
    Ajout @entity etc pour bdd sur items

--- j-and-d-back/src/main/java/com/projet/j_and_d/model/Item.java
+++ j-and-d-back/src/main/java/com/projet/j_and_d/model/Item.java
@@ -1,7 +1,21 @@
 package com.projet.j_and_d.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="item")
 public class Item {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	@Column(length = 180,nullable = false)
 	private String name;
+	@Column(length = 180,nullable = false)
 	private String description;
 
 	public Item(String name, String description) {
@@ -25,9 +39,18 @@ public class Item {
 		this.description = description;
 	}
 
+	
 	@Override
 	public String toString() {
 		return "Item [name=" + name + ", description=" + description + "]";
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
 	}
 
 }
