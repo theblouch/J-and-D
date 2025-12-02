2a2eaec4c70342d544ad1509667880d6687a1b52
Yaurine <laurine.desbats@gmail.com>
2025-12-02 14:58:34 +0100

    Création de AuthApiController
    :)

---
+++ j-and-d-back/src/main/java/com/projet/j_and_d/api/AuthApiController.java
@@ -0,0 +1,27 @@
+package com.projet.j_and_d.api;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.projet.j_and_d.api.request.AuthRequest;
+import com.projet.j_and_d.api.response.AuthResponse;
+import com.projet.j_and_d.security.service.SecurityService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthApiController {
+    private final SecurityService service;
+
+    public AuthApiController(SecurityService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public AuthResponse auth(@Valid @RequestBody AuthRequest request) {
+        return this.service.auth(request);
+    }
+}
