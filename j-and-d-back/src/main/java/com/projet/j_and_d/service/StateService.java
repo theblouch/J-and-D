46c40f2ef69d122ea7bee3dec97388658da294e3
theblouch <thibaud.reigner@gmail.com>
2025-12-02 10:03:21 +0100

    Correction de StateService

--- j-and-d-back/src/main/java/com/projet/j_and_d/service/StateService.java
+++ j-and-d-back/src/main/java/com/projet/j_and_d/service/StateService.java
@@ -7,29 +7,31 @@ public class StateService {
 
     public boolean canAttack(Creature creature) {
 
-        if (creature.getState(State.Stunned)){
-            return false;
+        if (creature.getState().contains(State.Stunned)) {
             System.out.println("Personnage Stunned");
+            return false;
         }
 
-        if (creature.getState(State.Asleep)){
-            return false;
+        if (creature.getState().contains(State.Asleep)) {
             System.out.println("Personnage Asleep");
+            return false;
         }
 
         return true;
     }
 
-    public boolean targetable(Creature creature){
-        if (creature.getState(State.Invisible)){
+    public boolean targetable(Creature creature) {
+        if (creature.getState().contains(State.Invisible)) {
+            System.out.println("Target is invisible");
             return false;
-            System.out.println("Non ciblable");
+        } else {
+            return true;
         }
-        else {return true;}
     }
 
     public Creature getTauntedByCreature(Creature creature) {
-        if (!creature.getState(State.Taunted)) return null;
+        if (!creature.getState(State.Taunted))
+            return null;
         return creature.getTauntedBy(); // attribut à créer
     }
 }
