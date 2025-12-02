5f6ace8c644c7a690b0994ca209356c9cb5e1697
Bayshunpu <nolwen.berton@outlook.fr>
2025-12-02 10:24:31 +0100

    Ajout fn levelUP
    Version simpliste juste ajout quand character tue npc via IDamage

--- j-and-d-back/src/main/java/com/projet/j_and_d/model/IDamage.java
+++ j-and-d-back/src/main/java/com/projet/j_and_d/model/IDamage.java
@@ -7,6 +7,14 @@ public interface IDamage {
         if (receiver.getHp() < 0) {
             receiver.setHp(0);
             receiver.setAlive(false);
+
+            //Si receiver NPC et sender character alors gain xp
+            if (receiver instanceof NPC && sender instanceof Character){
+                NPC npc = (NPC) receiver;
+                Character character = (Character) sender;
+
+                character.levelUp(npc.getXP());
+            }
         }
     }
 
