6431228bc03e9d9891c0ee6d5060432b748f6972
costejb <jeanbaptistecoste@orange.fr>
2025-12-02 22:33:16 +0100

    Le début angular correction

--- j-and-d-front/src/app/app.routes.ts
+++ j-and-d-front/src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomePage } from './page/home-page/home-page';
-import { LoginPage } from './page/utilisateur/login-page/login-page';
+import { LoginPage } from './page/user/login-page/login-page';
 import { authGuard } from './guard/auth-guard';
-import { InscriptionPage } from './page/utilisateur/inscription-page/inscription-page';
+import { InscriptionPage } from './page/user/inscription-page/inscription-page';
 
 export const routes: Routes = [
   { path: '', component: HomePage, canActivate: [authGuard] },
