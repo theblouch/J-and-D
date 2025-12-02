ac72a2e9a82bd6844dc9577b343689216fb9f41c
Bayshunpu <nolwen.berton@outlook.fr>
2025-12-02 16:40:11 +0100

    Navigation login
    not working mais tkt soon

--- j-and-d-front/src/app/app.config.ts
+++ j-and-d-front/src/app/app.config.ts
@@ -1,11 +1,18 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { apiUrlInterceptor } from './interceptor/api-url-interceptor';
+import { jwtHeaderInterceptor } from './interceptor/jwt-header-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(
+      withInterceptors([ apiUrlInterceptor, jwtHeaderInterceptor ])
+    )
   ]
 };
--- j-and-d-front/src/app/app.routes.ts
+++ j-and-d-front/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+//import { AuteurPage } from './page/auteur/auteur-page/auteur-page';
+import { HomePage } from './page/home-page/home-page';
+//import { LoginPage } from './page/utilisateur/login-page/login-page';
+import { authGuard } from './guard/auth-guard';
+//import { InscriptionPage } from './page/user/inscription-page/inscription-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomePage, canActivate: [ authGuard ] },
+    { path: 'home', component: HomePage, canActivate: [ authGuard ] },
+  //  { path: 'login', component: LoginPage },
+   // { path: 'inscription', component: InscriptionPage },
+    //{ path: 'livre', component: LivrePage, canActivate: [ authGuard ] },
+];
