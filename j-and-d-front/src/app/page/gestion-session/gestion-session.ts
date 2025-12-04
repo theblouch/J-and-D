e9d36f3c375e28d446a661c9d9eb563d79ffa18f
Yaurine <laurine.desbats@gmail.com>
2025-12-04 11:43:51 +0100

    Premiere version de la gestion des sessions
    faut mapper les boutons maintenant

--- j-and-d-front/src/app/page/gestion-session/gestion-session.ts
+++ j-and-d-front/src/app/page/gestion-session/gestion-session.ts
@@ -1,20 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Observable, of, switchMap } from 'rxjs';
+import { SessionDto } from '../../dto/session-dto';
+import { SessionService } from '../../service/session-service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-gestion-session',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './gestion-session.html',
   styleUrl: './gestion-session.css',
 })
 export class GestionSession implements OnInit {
   sessionId!: string;
+  session$!: Observable<SessionDto | null>;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+    private route: ActivatedRoute,
+    private sessionService: SessionService
+  ) { }
 
   ngOnInit(): void {
-    // Récupérer le paramètre 'sessionId'
+    // Récupère l'ID depuis l'URL
     this.sessionId = this.route.snapshot.paramMap.get('sessionId')!;
-    console.log('ID de la session :', this.sessionId);
+
+    // Charge la session depuis le service
+    this.session$ = this.sessionService.findById(Number(this.sessionId))
+      .pipe(
+        switchMap(session => of(session)) // convertit en Observable si nécessaire
+      );
+  }
+
+  // Méthodes pour les boutons
+  showNPCs(session: SessionDto) {
+    alert('Monstres : ' + (session.npcNames?.join(', ') || 'Aucun Monstre'));
+  }
+
+  showCharacters(session: SessionDto) {
+    alert('Characters : ' + (session.inscriptionCharacters?.join(', ') || 'Aucun personnage'));
+  }
+
+  rollDice() {
+    const result = Math.floor(Math.random() * 20) + 1; // D20
+    alert('Résultat du dé : ' + result);
   }
 }
