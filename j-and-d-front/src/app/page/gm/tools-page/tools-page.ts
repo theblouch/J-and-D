023ffe846c2475b263607349e1cee334f6ec664d
costejb <jeanbaptistecoste@orange.fr>
2025-12-04 10:32:52 +0100

    page tools

--- j-and-d-front/src/app/page/gm/tools-page/tools-page.ts
+++ j-and-d-front/src/app/page/gm/tools-page/tools-page.ts
@@ -1,11 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-tools-page',
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './tools-page.html',
   styleUrl: './tools-page.css',
 })
-export class ToolsPage {
-
-}
+export class ToolsPage {}
